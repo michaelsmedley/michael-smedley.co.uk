9ce623bae89eb1a076ccfa1d262f90611d31425d
michaelsmedley <mike@michael-smedley.co.uk>
2017-04-05 16:24:31 +0100

    Mobile first amends

--- app/assets/js/app/app.js
+++ app/assets/js/app/app.js
@@ -2,9 +2,9 @@ var myApp = angular.module('_michaelsmedleycouk',['ngRoute','ngAnimate']);
 
 myApp.run(["$rootScope","$http","$location",function($rootScope,$http,$location) {
     $rootScope.nav = [
-    {"name":"About","link":"/about"},
-    {"name":"Work","link":"/work"},
-    {"name":"Contact","link":"/contact"}
+    {"name":"About","link":"about"},
+    {"name":"Work","link":"work"},
+    {"name":"Contact","link":"contact"}
     ];
     $rootScope.$location = $location;
 }]);
