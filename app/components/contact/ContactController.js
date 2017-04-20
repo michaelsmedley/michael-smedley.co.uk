f8f842caccca840a4f20b11f40882377adb47d4e
michaelsmedley <mike@michael-smedley.co.uk>
2017-04-20 14:51:21 +0100

    Use a JSON array for links here

--- app/components/contact/ContactController.js
+++ app/components/contact/ContactController.js
@@ -3,4 +3,13 @@ myApp.controller("ContactController", ["$scope", "$routeParams", "$rootScope", f
      $scope.params = $routeParams;
 
      $rootScope.metaDesc = "here is the meta for the contact page";
+
+     $scope.links = [
+     {"label":"Twitter","link":"https://twitter.com/smdly"},
+     {"label":"Instagram","link":"https://www.instagram.com/mikesmedley/"},
+     {"label":"LinkedIn","link":"https://www.linkedin.com/in/smdly/"},
+     {"label":"Medium","link":"https://medium.com/@smdly/"},
+     {"label":"Codepen","link":"https://codepen.io/smdly/"},
+     {"label":"Github","link":"https://github.com/michaelsmedley/"}
+     ];
 }]);
